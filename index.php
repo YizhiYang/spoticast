7ed96d61f767031b0160f6967323b52b8c5723e8
Matt O'Connell <mattoconnell408@gmail.com>
2016-11-06 15:21:08 -0500

    ?

--- index.php
+++ index.php
@@ -8,16 +8,4 @@
 			<br><br><br><br><br><br><br><br><br><br>
 		</div>
 	</body>
-	<script>
-/*
-		$( document ).ready(function() {
-		    if (navigator.userAgent.match(/(iPod|iPhone|iPad)/)) {
-				$('.warning').text('Apple mobile devices don\'t support the features used in this app. :( Try it out on your computer or Android Device');
-				$('a').click(function(){ return false;});
-			} else {
-				$('.stat_report').text('Get Playlists');
-			}
-		});
-*/
-	</script>
 </html>
